a0239cceaae417437513be2e39e8ed5499f9257b
Victor Lourme <victor.lourme@icloud.com>
2020-09-22 16:00:13 +0200

    Initial Commit

---
+++ InvoiceKit/InvoiceKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceKitApp.swift
+//  InvoiceKit
+//
+//  Created by Victor Lourme on 22/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
